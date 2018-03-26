1ded006474d43638a8258a1a177df98d3c962af6
Coder-ACJHP <hexa.octabin@gmail.com>
2018-03-26 18:49:31 +0200

    initialize

--- src/com/coder/snake/model/Snake.java
+++ src/com/coder/snake/model/Snake.java
@@ -1,7 +1,57 @@
 package com.coder.snake.model;
 
+import com.coder.snake.view.GamePanel;
+
 public class Snake {
 
-	
+	GamePanel main;
+
+	public int direction = 0;
+	public int length = 2;
+
+	public int snakeX[] = new int[main.WIDTH * main.HEIGHT];
+	public int snakeY[] = new int[main.WIDTH * main.HEIGHT];
+
+	public Snake(int x0, int y0, int x1, int y1) {
+		snakeX[0] = x0;
+		snakeY[0] = y0;
+		snakeX[1] = x1;
+		snakeY[1] = y1;
+	}
+
+	@SuppressWarnings("static-access")
+	public void move() {
+
+		for (int d = length; d > 0; d--) {
+			snakeX[d] = snakeX[d - 1];
+			snakeY[d] = snakeY[d - 1];
+		}
+
+		if (direction == 0)
+			snakeX[0]++;
+		if (direction == 1)
+			snakeY[0]++;
+		if (direction == 2)
+			snakeX[0]--;
+		if (direction == 3)
+			snakeY[0]--;
+
+		for (int d = length - 1; d > 0; d--) {
+			if ((snakeX[0] == snakeX[d]) & (snakeX[0] == snakeY[d]))
+				length = d - 2;
+		}
+
+		if (snakeX[0] > main.WIDTH)
+			snakeX[0] = 0;
+		if (snakeX[0] < 0)
+			snakeX[0] = main.WIDTH - 1;
+		if (snakeY[0] > main.HEIGHT - 1)
+			snakeY[0] = 0;
+		if (snakeY[0] < 0)
+			snakeY[0] = main.HEIGHT - 1;
+
+		if (length < 2)
+			length = 2;
+	}
 	
 }
--- src/com/coder/snake/view/GamePanel.java
+++ src/com/coder/snake/view/GamePanel.java
@@ -1,6 +1,10 @@
 package com.coder.snake.view;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
@@ -13,12 +17,27 @@ import java.awt.event.KeyListener;
 import javax.swing.ImageIcon;
 import javax.swing.JColorChooser;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-import com.coder.snake.model.Direction;
+import com.coder.snake.model.Apple;
+import com.coder.snake.model.Snake;
 
 public class GamePanel extends JPanel implements ActionListener {
 	
-	
+	/*
+	 * https://forum.tutorials7.com/1527/how-to-create-snake-game-in-java-with-
+	 * intellij-idea-code
+	 */
+	public static final int CELL_SIZE = 10;
+	public static final int SCALE = 32;
+	public static final int WIDTH = 20;
+	public static final int HEIGHT = 20;
+	public static final int SPEED = 4;
+
+	Apple apple = new Apple((int) (Math.random() * WIDTH), (int) (Math.random() * HEIGHT));
+	Snake snake = new Snake(10, 10, 9, 10);
+	Timer timer = new Timer(1000 / SPEED, this);
+
 	private boolean mute = false;
 	private Color flexColor = Color.WHITE;
 	private ControlPanel currentControlPanel;
@@ -33,9 +52,38 @@ public class GamePanel extends JPanel implements ActionListener {
 	    this.addFocusListener(customGetFocus());
 	    this.addKeyListener(customKeyAdapter());
 	    controlPanel.addActionToButtons(this);
-
+		timer.start();
 	}
 	
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		final Graphics2D graphics2d = (Graphics2D) g;
+		graphics2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+		drawlines(graphics2d);
+
+		graphics2d.setColor(Color.BLACK);
+		for (int xx = 0; xx <= WIDTH * SCALE; xx += SCALE) {
+			graphics2d.drawLine(xx, 0, xx, HEIGHT * SCALE);
+		}
+
+		for (int yy = 0; yy <= HEIGHT * SCALE; yy += SCALE) {
+			graphics2d.drawLine(0, yy, WIDTH * SCALE, yy);
+		}
+
+		for (int d = 0; d < snake.length; d++) {
+			graphics2d.setColor(Color.GREEN);
+			graphics2d.fillRect(snake.snakeX[d] * SCALE + 1, snake.snakeY[d] * SCALE + 1, SCALE - 1, SCALE - 1);
+		}
+
+		graphics2d.setColor(Color.BLACK);
+		graphics2d.fillRect(apple.posX * SCALE + 1, apple.posY * SCALE + 1, SCALE - 1, SCALE - 1);
+	}
+
+
 	public void startGame() {
 		
 	}
@@ -56,28 +104,16 @@ public class GamePanel extends JPanel implements ActionListener {
 		repaint();
 	}
 
+	public void drawlines(Graphics2D g) {
 
-	public Enum<Direction> moveToLeft() {
-		return Direction.LEFT;
-		
+		for (int x = 0; x <= getWidth(); x += CELL_SIZE) {
+			for (int y = 0; y <= getHeight(); y += CELL_SIZE) {
+				g.setStroke(new BasicStroke(1));
+				g.setColor(Color.GRAY.brighter());
+				g.drawRect(x, y, CELL_SIZE, CELL_SIZE);
+			}
+		}
 	}
-
-
-	public Enum<Direction> moveToRight() {
-		return Direction.RIGHT;
-		
-	}
-
-
-	public Enum<Direction> moveToUp() {
-		return Direction.UP;
-	}
-
-
-	public Enum<Direction> moveToDown() {
-		return Direction.DOWN;
-	}
-
 
 	//When this panel lost focus focus on again. 
 	private FocusListener customGetFocus() {
@@ -147,19 +183,14 @@ public class GamePanel extends JPanel implements ActionListener {
 			changeBackground(flexColor);
 			break;
 		case "start":
-			startGame();
 			break;
 		case "right":
-			moveToRight();
 			break;
 		case "left":
-			moveToLeft();
 			break;
 		case "up":
-			moveToUp();
 			break;
 		case "down":
-			moveToDown();
 			break;
 		default:
 			break;
